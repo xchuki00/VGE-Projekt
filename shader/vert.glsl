8eb7b3aa7f099db5993c0fe1e3405f1f503df307
Patrik Chukir <p.chukir@gmail.com>
2019-04-30 13:09:37 +0200

    clean

--- shader/vert.glsl
+++
@@ -1,8 +0,0 @@
-#version 440 core
-layout (location = 0) in vec3 a_position;
-layout (location = 0) in int indecies;
-uniform mat4 mvp;
-void main() {
-   coord = a_position.xy;
-   gl_Position = mvp*vec4(a_position, 1);
-}
